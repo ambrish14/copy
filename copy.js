34320a56f1d870269caa13fe790f1b3096b6cb7c
Ambrish Chaurasia <69149051+ambrish14@users.noreply.github.com>
2026-06-01 19:52:08 +0530

    how to handle bigger file   by reading in buffer and copy to the new destination file and handling the memory usage and speed

--- copy.js
+++ copy.js
@@ -2,12 +2,62 @@ const fs = require("node:fs/promises");
 //speed
 //space
 
+// file size copied 1gb
+// Memory usage :1GB
+// Execution time :1.663s
+// (async () => {
+//   console.time("copy");
+//   const destFile = await fs.open("text-copy.txt", "w");
+//   // const result = await fs.readFile("text-gigantic.txt");
+//   const result = await fs.readFile("text-big.txt");
+
+//   await destFile.write(result);
+//   console.timeEnd("copy");
+// })();
+
+// file size copied 1gb
+// Memory usage :14mb
+// Execution time :3.869
+
 (async () => {
   console.time("copy");
-  const destFile = await fs.open("text-copy.txt", "w");
-  // const result = await fs.readFile("text-gigantic.txt");
-  const result = await fs.readFile("text-big.txt");
+  const srcFile = await fs.open("text-gigantic.txt", "r");
+  // const srcFile = await fs.open("text-big.txt", "r");
 
-  await destFile.write(result);
+  const destFile = await fs.open("text-copy.txt", "w");
+
+  let bytesRead = -1;
+  while (bytesRead !== 0) {
+    const readResult = await srcFile.read();
+    bytesRead = readResult.bytesRead;
+    // console.log(readResult);
+
+    if (bytesRead !== 16384) {
+      const indexNotFilled = readResult.buffer.indexOf(0);
+      const newBuffer = Buffer.alloc(indexNotFilled);
+      readResult.buffer.copy(newBuffer, 0, 0, indexNotFilled);
+      destFile.write(newBuffer);
+    } else {
+      destFile.write(readResult.buffer);
+    }
+  }
+
+  // const readResult = await srcFile.read();
+  // console.log(readResult);
+
+  console.timeEnd("copy");
+})();
+
+// file size copied 1gb
+// Memory usage :14mb
+// Execution time :3.869
+
+(async () => {
+  console.time("copy");
+  const srcFile = await fs.open("text-gigantic.txt", "r");
+  // const srcFile = await fs.open("text-big.txt", "r");
+
+  const destFile = await fs.open("text-copy.txt", "w");
+
   console.timeEnd("copy");
 })();
