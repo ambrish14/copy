995817bdd5b2130bec21c4805232d9650252d195
Ambrish Chaurasia <69149051+ambrish14@users.noreply.github.com>
2026-06-01 21:42:52 +0530

    using  stream method by first creating read and write stream instance  and piping the data through the pipe method

--- copy.js
+++ copy.js
@@ -17,47 +17,59 @@ const fs = require("node:fs/promises");
 
 // file size copied 1gb
 // Memory usage :14mb
-// Execution time :3.869
+// Execution time :3.869s
 
-(async () => {
-  console.time("copy");
-  const srcFile = await fs.open("text-gigantic.txt", "r");
-  // const srcFile = await fs.open("text-big.txt", "r");
+// (async () => {
+//   console.time("copy");
+//   const srcFile = await fs.open("text-gigantic.txt", "r");
+//   // const srcFile = await fs.open("text-big.txt", "r");
 
-  const destFile = await fs.open("text-copy.txt", "w");
+//   const destFile = await fs.open("text-copy.txt", "w");
 
-  let bytesRead = -1;
-  while (bytesRead !== 0) {
-    const readResult = await srcFile.read();
-    bytesRead = readResult.bytesRead;
-    // console.log(readResult);
+// let bytesRead = -1;
+// while (bytesRead !== 0) {
+//   const readResult = await srcFile.read();
+//   bytesRead = readResult.bytesRead;
+//   // console.log(readResult);
 
-    if (bytesRead !== 16384) {
-      const indexNotFilled = readResult.buffer.indexOf(0);
-      const newBuffer = Buffer.alloc(indexNotFilled);
-      readResult.buffer.copy(newBuffer, 0, 0, indexNotFilled);
-      destFile.write(newBuffer);
-    } else {
-      destFile.write(readResult.buffer);
-    }
-  }
+//   if (bytesRead !== 16384) {
+//     const indexNotFilled = readResult.buffer.indexOf(0);
+//     const newBuffer = Buffer.alloc(indexNotFilled);
+//     readResult.buffer.copy(newBuffer, 0, 0, indexNotFilled);
+//     destFile.write(newBuffer);
+//   } else {
+//     destFile.write(readResult.buffer);
+//   }
+// }
 
-  // const readResult = await srcFile.read();
-  // console.log(readResult);
+//   // const readResult = await srcFile.read();
+//   // console.log(readResult);
 
-  console.timeEnd("copy");
-})();
+//   console.timeEnd("copy");
+// })();
 
 // file size copied 1gb
 // Memory usage :14mb
-// Execution time :3.869
+// Execution time :3.15s
 
 (async () => {
   console.time("copy");
-  const srcFile = await fs.open("text-gigantic.txt", "r");
+  const srcFile = await fs.open("text-big.txt", "r");
   // const srcFile = await fs.open("text-big.txt", "r");
 
   const destFile = await fs.open("text-copy.txt", "w");
+
+  const readStream = srcFile.createReadStream();
+  const writeStream = destFile.createWriteStream();
+
+  // readStream.pipe(writeStream);
+
+  // We use a Promise to wait for the pipe to finish
+  await new Promise((resolve, reject) => {
+    readStream.pipe(writeStream);
+    writeStream.on("finish", resolve);
+    writeStream.on("error", reject);
+  });
 
   console.timeEnd("copy");
 })();
