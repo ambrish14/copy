575b567007be8fb13177b30c345a6ec70c86e37c
Ambrish Chaurasia <69149051+ambrish14@users.noreply.github.com>
2026-06-01 17:49:40 +0530

    that  .readFile will going to convert the text file in buffer

--- copy.js
+++ copy.js
@@ -1,1 +1,13 @@
 const fs = require("node:fs/promises");
+//speed
+//space
+
+(async () => {
+  console.time("copy");
+  const destFile = await fs.open("text-copy.txt", "w");
+  // const result = await fs.readFile("text-gigantic.txt");
+  const result = await fs.readFile("text-big.txt");
+
+  await destFile.write(result);
+  console.timeEnd("copy");
+})();
